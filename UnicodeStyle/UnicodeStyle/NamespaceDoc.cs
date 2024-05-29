23ed233c3d0c93c1fa2d4d6dc0f3c27ba238d667
wherewhere <qq2518766683@outlook.com>
2024-05-29 20:10:49 +0800

    更新协议到 CC-BY-SA-4.0
    修正 Logo

--- UnicodeStyle/UnicodeStyle/NamespaceDoc.cs
+++ UnicodeStyle/UnicodeStyle/NamespaceDoc.cs
@@ -6,14 +6,14 @@ namespace UnicodeStyle
     /// <summary>
     /// <para>A Styler to format text using Unicode characters.</para>
     /// <para>Source from the JavaScript version of Unicode Text Styler by Andrew West
-    /// at <see href="http://www.babelstone.co.uk/Unicode/text.js"/> under a
+    /// at <see href="https://www.babelstone.co.uk/Unicode/text.js"/> under a
     /// Creative Commons Attribution-ShareAlike 3.0f Unported License.</para>
     /// <para>Format text by using <see cref="UnicodeStyle.StyleConvert(string, UnicodeStyles)"/>.</para>
     /// </summary>
     /// <remarks>
     /// <para>Creative Commons License:</para>
-    /// <para><see href="http://creativecommons.org/licenses/by-sa/3.0f/">CC BY-SA 3.0f</see></para>
-    /// <para>Copyright © 2017 - 2023 wherewhere. All Rights Reserved.</para>
+    /// <para><see href="https://creativecommons.org/licenses/by-sa/4.0/">CC-BY-SA-4.0</see></para>
+    /// <para>Copyright © 2017 - 2024 wherewhere. All Rights Reserved.</para>
     /// </remarks>
     [CompilerGenerated]
     internal class NamespaceDoc;
